74db72872226b1cb1dee0d421a2185cfce3044cb
badshahshaikh <40516664+badshahshaikh@users.noreply.github.com>
2022-03-10 14:45:06 +0530

    Update backend-search.php

--- backend-search.php
+++ backend-search.php
@@ -19,21 +19,14 @@ if( isset($_REQUEST["gender"]) )
 }
 
 
-if( $_REQUEST["below_ssc"] == 'on' || $_REQUEST["ssc_pass"] == 'on' || $_REQUEST["hsc_pass"] == 'on' || $_REQUEST["under_graduate"] == 'on' || $_REQUEST["graduate"] == 'on' || $_REQUEST["post_graduate"] == 'on' || $_REQUEST["phd"] == 'on' )
-{
+if(!empty($_POST['qual_std'])) {
 
-	$qualification_arr = array();
-
-	( $_REQUEST["below_ssc"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'Below SSC'") : '';
-	( $_REQUEST["ssc_pass"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'SSC Passed'") : '';
-	( $_REQUEST["hsc_pass"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'HSC Passed'") : '';
-	( $_REQUEST["under_graduate"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'Under Graduate'") : '';
-	( $_REQUEST["graduate"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'Graduate'") : '';
-	( $_REQUEST["post_graduate"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'Post Graduate'") : '';
-	( $_REQUEST["phd"] == 'on' ) ? array_push($qualification_arr,"cd_qualification_std = 'PHD'") : '';
-	$sel_qual = implode(" OR ",$qualification_arr);
-
-	$sql .= ' AND ( '.$sel_qual.' ) ';
+	$qual_std_arr=array();
+	foreach($_POST['qual_std'] as $value){
+		array_push($qual_std_arr,$value) ;
+	}
+	$brandii=implode("','",$qual_std_arr); 
+	$sql .= "AND cd_qualification_std IN ('".$brandii."')";
 
 }
 
@@ -48,54 +41,37 @@ if(!empty($_POST['qual_strm'])) {
 
 }
 
-if( $_REQUEST["fresher"] == 'on' || $_REQUEST["below1year"] == 'on' || $_REQUEST["onetotwoyears"] == 'on' || $_REQUEST["threestofiveyears"] == 'on' || $_REQUEST["sixtonineyears"] == 'on' || $_REQUEST["tenplusyears"] == 'on' )
-{
-	$exp_arr = array();
-	( $_REQUEST["fresher"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = 'Fresher'") : '';
-	( $_REQUEST["below1year"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = 'below-1 year'") : '';
-	( $_REQUEST["1to2year"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = '1-2 years'") : '';
-	( $_REQUEST["3to5year"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = '3-5 years'") : '';
-	( $_REQUEST["6to9year"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = '6-9 years'") : '';
-	( $_REQUEST["10plusyear"] == 'on' ) ? array_push($exp_arr,"cd_fresher_intern_exp = '10+ years'") : '';
-	$sel_exp = implode(" OR ",$exp_arr);
-	$sql .= ' AND ( '.$sel_exp.' ) ';		
+if(!empty($_POST['exp_c'])) {
+
+	$exp=array();
+	foreach($_POST['exp_c'] as $value){
+		array_push($exp,$value) ;
+	}
+	$brandii=implode("','",$exp); 
+	$sql .= "AND cd_fresher_intern_exp IN ('".$brandii."')";
+
 }
 
-if( $_REQUEST["lng_hindi"] == 'on' || $_REQUEST["lng_marathi"] == 'on' || $_REQUEST["lng_eng"] == 'on' || $_REQUEST["lng_tamil"] == 'on' || $_REQUEST["lng_urdu"] == 'on' || $_REQUEST["lng_guj"] == 'on' )
-{
-	$language_sql = 'AND cd_languages LIKE "%'; 
-	$language_arr = array();
-	
-	( $_REQUEST["lng_hindi"] == 'on' ) ? array_push($language_arr,"Hindi") : '';
-	( $_REQUEST["lng_marathi"] == 'on' ) ? array_push($language_arr,"Marathi")  : '';
-	( $_REQUEST["lng_eng"] == 'on' ) ? array_push($language_arr,"English") : '';
-	( $_REQUEST["lng_tamil"] == 'on' ) ? array_push($language_arr,"Tamil") : '';
-	( $_REQUEST["lng_urdu"] == 'on' ) ? array_push($language_arr,"Urdu") : '';
-	( $_REQUEST["lng_guj"] == 'on' ) ? array_push($language_arr,"Gujrati") : '';
-	$sel_lang = implode("%",$language_arr);
+if(!empty($_POST['lang_'])) {
 
-	$sql .= $language_sql.$sel_lang.'%"';
+	$lang_=array();
+	foreach($_POST['lang_'] as $value){
+		array_push($lang_,$value) ;
+	}
+	$brandii=implode("%",$lang_); 
+	$sql .= "AND cd_languages LIKE '%".$brandii."%'";
 }
 
 
-if( $_REQUEST["Intern"] == 'on' || $_REQUEST["Executive"] == 'on' || $_REQUEST["Sr_Executive"] == 'on' || $_REQUEST["Team_leader"] == 'on' || $_REQUEST["Trainer"] == 'on' || $_REQUEST["Assistant_Manager"] == 'on' || $_REQUEST["Manager"] == 'on' || $_REQUEST["Sr_Manager"] == 'on' || $_REQUEST["Head"] == 'on' || $_REQUEST["AVP"] == 'on' || $_REQUEST["VP"] == 'on'  )
-{
+if(!empty($_POST['exp_des'])) {
 
-	$des_arr = array();
+	$exp_des=array();
+	foreach($_POST['exp_des'] as $value){
+		array_push($exp_des,$value) ;
+	}
 
-	( $_REQUEST["Intern"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Internship'") : '';
-	( $_REQUEST["Executive"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Executive'") : '';
-	( $_REQUEST["Sr_Executive"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Sr Executive'") : '';
-	( $_REQUEST["Team_leader"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Team leader'") : '';
-	( $_REQUEST["Trainer"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Trainer'") : '';
-	( $_REQUEST["Assistant_Manager"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Assistant Manager'") : '';
-	( $_REQUEST["Manager"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Manager'") : '';
-	( $_REQUEST["Sr_Manager"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Sr Manager'") : '';
-	( $_REQUEST["Head"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'Head'") : '';
-	( $_REQUEST["AVP"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'AVP'") : '';
-	( $_REQUEST["VP"] == 'on' ) ? array_push($des_arr,"cd_exp_designation = 'VP'") : '';
-	$sel_des = implode(" OR ",$des_arr);
-	$sql .= ' AND ( '.$sel_des.' ) ';	
+	$brandii=implode("','",$exp_des); 
+	$sql .= "AND cd_exp_designation LIKE ('".$brandii."')";
 
 }
 
@@ -188,8 +164,8 @@ if(!empty($_POST['cd'])) {
 	$sql .= "AND cd_documents LIKE '%".$cd."%'";
 
 }
-echo $sql;
-exit();
+// echo $sql;
+// exit();
 	$result = $link->query($sql);
 
 	echo '<br> total = '.mysqli_num_rows ( $result ).'<br>';
@@ -209,6 +185,7 @@ exit();
 	  <th>Job Profiel One</th>
 	  <th>Job Designation One One</th>
 	  <th>Industry</th>
+	  <th>Document</th>
 	</tr>
 	<tbody>";
 
@@ -229,6 +206,7 @@ exit();
 		<td> ". $row['cd_job_profile_one'] . "</td>
 		<td> ". $row['cd_job_designation_one'] . "</td>
 		<td> ". $row['cd_exp_job_industry'] . "</td>
+		<td> ". $row['cd_documents'] . "</td>
 		
 		
 		
